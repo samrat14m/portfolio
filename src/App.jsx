b136c5dcbc438880498fcfe8329e77f1d814150e
SAURABH MAURYA <saurabhmaurya14031997@gmail.com>
2022-09-18 21:44:45 +0530

    styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import Header from "./components/header/Header";
+import NavBar from "./components/nav/NavBar";
+import About from "./components/about/About";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <About />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
